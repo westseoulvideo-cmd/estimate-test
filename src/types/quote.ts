a4cec03eb0fe81eca1c2b2208e001da66c4346ce
gyeongyun <westseoulvideo@gmail.com>
2026-05-07 09:55:16 +0900

    refactor: 견적서 타입 재작성 (Excel 기반 구조)

--- src/types/quote.ts
+++ src/types/quote.ts
@@ -1,21 +1,50 @@
+export type ShootingType = 'photo' | 'video' | 'photo+video';
+export type ShootingHours = '4h' | '8h';
+export type TravelLocation = 'seoul' | 'chungcheong' | 'jeonla' | 'jeju' | 'overseas';
+export type IntroOutroType = 'none' | 'free' | 'premium';
+
+export interface ClientInfo {
+  name: string;
+  position: string;
+  department: string;
+  contact: string;
+  company: string;
+  projectName: string;
+  projectDate: string;
+  projectLocation: string;
+  requirements: string;
+  quoteDate: string;
+  refs: string[];
+}
+
 export interface QuoteInput {
+  shootingType: ShootingType;
+  shootingHours: ShootingHours;
+  aerial: boolean;
   shootingCount: number;
+  compositionMinutes: number;
+  travelLocation: TravelLocation;
   editMinutes: number;
-  drone: boolean;
-  extraCrew: number;
+  entertainmentEffect: boolean;
+  shortsMinutes: number;
+  shortsEntertainmentEffect: boolean;
+  introOutro: IntroOutroType;
+  aiVideoMinutes: number;
+  episodeCount: number;
+  additionalWork: boolean;
 }
 
 export interface QuoteResult {
   shootingFee: number;
+  compositionFee: number;
+  travelFee: number;
   editingFee: number;
-  droneFee: number;
-  extraCrewFee: number;
+  shortsEditingFee: number;
+  introOutroFee: number;
+  aiVideoFee: number;
+  subtotalPerEpisode: number;
+  subtotal: number;
+  discount: number;
   total: number;
-}
-
-export interface QuoteRequest {
-  clientName: string;
-  refs: string[];
-  input: QuoteInput;
-  result: QuoteResult;
+  isTravelNegotiable: boolean;
 }
